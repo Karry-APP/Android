9d76ed631a290c2f6710d985d2f50ec68d79e2b9
LeoGvs <44402734+LeoGvs@users.noreply.github.com>
2019-04-23 16:17:20 +0200

    Kar 36 (#38)
    * Ajout du tab layout du chat container #36
    
    * Ajout des fichiers manquants #36

--- app/src/main/java/myapp/com/karry/fragments/chat/ChatMessageFragment.kt
+++ app/src/main/java/myapp/com/karry/fragments/chat/ChatMessageFragment.kt
@@ -1,4 +1,16 @@
 package myapp.com.karry.fragments.chat
 
-class ChatMessageFragment {
-}
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import myapp.com.karry.R
+
+class ChatMessageFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v: View = inflater.inflate(R.layout.fragment_chat_message, container, false)
+        return v
+    }
