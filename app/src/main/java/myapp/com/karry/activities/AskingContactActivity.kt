5e9741f9c5ff3a4e49cf67ace9e7a9495369fa6b
Nicolas <nicolas@eccmperformance.com>
2019-06-03 11:46:49 +0200

    Make askingContact activity

---
+++ app/src/main/java/myapp/com/karry/activities/AskingContactActivity.kt
@@ -0,0 +1,13 @@
+package myapp.com.karry.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import myapp.com.karry.R
+
+class AskingContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_asking_contact)
+    }
+}
--- app/src/main/java/myapp/com/karry/fragments/chat/ChatContactFragment.kt
+++ app/src/main/java/myapp/com/karry/fragments/chat/ChatContactFragment.kt
@@ -1,11 +1,14 @@
 package myapp.com.karry.fragments.chat
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_chat_contact.view.*
 import myapp.com.karry.R
+import myapp.com.karry.activities.AskingContactActivity
 
 class ChatContactFragment : Fragment() {
 
@@ -13,6 +16,12 @@ class ChatContactFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val v: View = inflater.inflate(R.layout.fragment_chat_contact, container, false)
+
+        v.askingContactRow.setOnClickListener {
+            val intent = Intent(this.context, AskingContactActivity::class.java)
+            startActivity(intent)
+        }
+
         return v
     }
 }
